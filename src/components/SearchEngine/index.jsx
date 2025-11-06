2e558cbfe097688b6b005ed68d022629d2b81d9b
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-11-06 21:21:21 +0300

    fix_search_engine

--- src/components/SearchEngine/index.jsx
+++ src/components/SearchEngine/index.jsx
@@ -1,8 +1,17 @@
 import style from './style.module.css';
+import SearchIcon from '../../assets/search.svg';
 
-
-export default function SearchEngine () {
-    return (
-        <input className={style.search} type="search" placeholder='Найти товары' />
-    )
+export default function SearchEngine() {
+  return (
+    <label htmlFor="searchInput" className={style.search}>
+      <img src={SearchIcon} alt="Иконка для поля ввода" />
+      <input
+        className={style.searchInput}
+        type="search"
+        placeholder="Найти товары"
+        id="searchInput"
+        name="searchInput"
+      />
+    </label>
+  );
 }
