02dba2f59f7d2f2d697c6ed3a12618c53ad2e9aa
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-11-23 21:32:24 +0300

    fix design profile

--- src/components/ProfileContainer/index.jsx
+++ src/components/ProfileContainer/index.jsx
@@ -1,4 +1,5 @@
 import style from './style.module.css'
+import avatarIcon from '../../assets/avatar.svg'
 
 export default function ProfileContainer ({ person, setPerson }) {
     const handleLogout = () => {
@@ -10,13 +11,18 @@ export default function ProfileContainer ({ person, setPerson }) {
         <section className={style.container}>
             <h1 className={style.container__title}>Профиль</h1>
             <div className={style.data}>
-                Данные пользователя
-                <ul className={style.data__items}>
-                    {person.map((user, index) => (
-                        <li className={style.data__item} key={index}>{user.email}</li>
-                    ))}
-                </ul>
-                <button className={style.resetData} onClick={handleLogout}>Выйти из профиля</button>
+                <h2 className={style.subtitle}>Данные пользователя</h2> 
+                <div className={style.dataContainer}>
+                    <img className={style.imgProfile} src={avatarIcon} alt="Изображение пользователя" />
+                    <div className={style.containerDesc}>
+                        <ul className={style.data__items}>
+                            {person.map((user, index) => (
+                                <li className={style.data__item} key={index}>{user.email}</li>
+                            ))}
+                        </ul>
+                        <button className={style.resetData} onClick={handleLogout}>Выйти из профиля</button>
+                    </div>
+                </div>
             </div>
         </section>
     )
